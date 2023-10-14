5c2344bb200fb1c1ebc41924cf75c2621cbb4c39
unknown <vladfedko35@gmail.com>
2023-10-14 22:45:20 +0300

    Add controller for ping

---
+++ DogAPI/DogAPI/Controllers/CommonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DogAPI.Controllers;
+
+[ApiController]
+[Route("/")]
+public class CommonController : ControllerBase
+{
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("Dogshouseservice.Version1.0.1");
+    }
+}
